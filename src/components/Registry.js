baf335385415447f8064d8814ecd9a4861306d44
Daniel de Jesus <danieldejesus8745@gmail.com>
2020-07-05 23:19:38 -0300

    Alteração na forma do componente Registry

--- src/components/Registry.js
+++ src/components/Registry.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import Menu from './Menu';
 import './css/registry.css';
+import Menu from './Menu';
 
-function Registry() {
-    return (
-      <div className="registry">
-          <Menu />
+class Registry extends React.Component {
 
-          <div className="div-registry-form">
-            <p>Cadastro de aluno</p>
-            <form className="registry-form">
-              <input type="text" placeholder="Nome do aluno" />
-              <input type="text" placeholder="Data de nascimento" />
-              <input type="text" placeholder="Endereço" />
-              <input type="text" placeholder="Responsável" />
-              <input type="submit" value="Matricular" />
-            </form>
-          </div>
-      </div>
-    );
-  }
-  
-  export default Registry;
+    formSubmit(e) {
+        e.preventDefault();
+        alert('validação');
+    }
+
+    putBorder(input) {
+        const inputField = document.querySelector(`#${input.target.id}`);
+        inputField.style.border = '1px solid blue';
+    }
+
+    removeBorder(input) {
+        const inputField = document.querySelector(`#${input.target.id}`);
+        inputField.style.border = '1px solid lightgray';
+    }
+
+    render() {
+        return (
+            <div className="registry">
+                <Menu />
+                <div className="div-registry-form">
+                    <p>Cadastro de aluno</p>
+                    <form className="registry-form" onSubmit={ this.formSubmit }>
+                        <input type="text" placeholder="Nome do aluno" id="name" onFocus={this.putBorder}
+                        onBlur={this.removeBorder} required />
+                        <input type="text" placeholder="Data de nascimento" id="born" onFocus={this.putBorder}
+                        onBlur={this.removeBorder} required />
+                        <input type="text" placeholder="Endereço" id="address" onFocus={this.putBorder}
+                        onBlur={this.removeBorder} required />
+                        <input type="text" placeholder="Responsável" id="sponsor" onFocus={this.putBorder}
+                        onBlur={this.removeBorder} required />
+                        <input type="submit" value="Maricular" />
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Registry;
